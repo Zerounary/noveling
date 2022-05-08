7104c19f1059d98cba2d61e34276919472838d45
Zerounary <850657663@qq.com>
2022-05-08 18:05:44 +0800

    添加名字生成页面

---
+++ src/pages/name/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 class="text-3xl">生成姓名</h1>
+    <button @click="clickGen" class="border px-3 py-1 rounded">生成</button>
+    <input class="border" v-model="nameNum" />
+    <ul>
+      <li v-for="name in nameList" :key="name">{{ name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import randomName from '@/utils/random-name';
+import { ref } from 'vue';
+const nameList = ref([]);
+const nameNum = ref(10);
+
+let clickGen = () => {
+  nameList.value = [];
+  while (nameList.value.length < nameNum.value) {
+    nameList.value.push(randomName.getMaleName(true));
+  }
+  nameList.value = nameList.value.sort((a,b) => a.length - b.length);
+  nameList.value = nameList.value.sort((a,b) => a[0] - b[0]);
+};
+</script>
+
+<style scoped></style>
